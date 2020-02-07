bd88b89530cd186a3dd7b591e5ac35ef56999c85
sekonioluwaseun@gmail.com <sekonioluwaseun@gmail.com>
2020-02-07 15:53:10 +0100

    Bugs to fix

--- app/Http/Controllers/Auth/API/AuthController.php
+++ app/Http/Controllers/Auth/API/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
         ];
  
         if (auth()->attempt($credentials)) {
-            $token = auth()->user()->createToken('activelearning')->accessToken;
+            // $token = auth()->user()->createToken('activelearning')->accessToken;
             dd(auth()->user());
             return response()->json(['token' => $token], 200);
         } else {
